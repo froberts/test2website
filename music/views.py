526f5dd6da0b309706f2d771f0daad2e5e736fe7
Frank Roberts <frank_m_roberts@hotmail.com>
2019-02-09 18:15:42 -0800

    Frank made a change

--- music/views.py
+++ music/views.py
@@ -6,7 +6,7 @@ from django.contrib.auth import authenticate, login
 from django.views.generic import View
 from .models import Album
 from .forms import UserForm
-# comment  see this
+# comment  see this more
 
 
 class IndexView(generic.ListView):
